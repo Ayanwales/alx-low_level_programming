895c81b838d251bae2d92c904f46dd56090b94ff
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-12-27 11:29:56 -0800

    added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,14 +4,14 @@
 /**
  * main -Entry point
  *
- * Description:"with proper grammar,but the outcome is a piece of art,"
+ * Description:"with proper grammar, but the outcome is a piece of art,"
  *
  * Return : Always 0 (Success)
  */
 int main(void)
 {
 
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
