605e2836065ea8929b8615cd62e5257525a0fe3a
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-12-29 01:04:41 -0800

    added

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,15 +12,14 @@ int main(void)
 {
 	int i = 97;
 
-	while( i < 123 )
+	while (i < 123)
 	{
-		if ( i != 101 && i != 113)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
 		i++;
 	}
 	putchar(10);
-	
-	return(0);
+	return (0);
 }
