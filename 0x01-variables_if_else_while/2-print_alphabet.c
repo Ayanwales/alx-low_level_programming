a6073d8e14291482905af3cd7a314278285c8121
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-12-29 00:33:12 -0800

    added

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+
 /**
- * main -print the letters of the alphabet
+ * main - print the letters of the alphabet
  *
  * Description: print the letters of the alphabet
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+ * /
+
+ int main(void)
 {
 	int 1 = 97;
+
 	while ( i < 123)
 	{
 		putchar(i);
 		i++;
 	}
 	putchar(10);
+
 	return(0);
 }
