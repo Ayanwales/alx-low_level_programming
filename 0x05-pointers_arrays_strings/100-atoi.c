2dbe45e6516bc2576b67846793a44a1e9099e396
Ayanwales <Ayanwalesulaymon@gmail.com>
2023-01-06 02:53:09 -0800

    added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,8 @@
  *
  * Return: the int converted from the string
  */
-  int _atoi(char *s)
+
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -30,9 +31,9 @@
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n* 10 + digit;
+			n = n * 10 + digit;
 			 f = 1;
-			 if (s[i + 1] < '0' || s[i + 1] > '9')
+				 if (s[i + 1] < '0' || s[i + 1] > '9')
 				 break;
 			 f = 0;
 		}
@@ -40,7 +41,7 @@
 		i++;
 	}
 
-	if (f ==0)
+	if (f == 0)
 		return (0);
 
 	return (n);
