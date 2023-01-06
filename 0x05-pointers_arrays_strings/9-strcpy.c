9aed09c7394552f98d35b95b18392b9a861cf681
Ayanwales <Ayanwalesulaymon@gmail.com>
2023-01-06 02:47:58 -0800

    added

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,9 +3,9 @@
 /**
  * _strcpy - copies the string pointed to by src into dest
  * @dest: destination
- * @src: source
- * Return:char with copy of string 
- */
+ *@src: source
+*Return:char with copy of string 
+*/
 
 char *_strcpy(char *dest,  char *src)
 {
