3d7751587c33518309515f38b9bcfb23e5cc65c1
Ayanwales <Ayanwalesulaymon@gmail.com>
2023-01-18 08:34:07 -0800

    added

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,52 @@
+#include <stdio.h>
 #include "function_pointers.h"
-#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of ponters to the arguments
- *
- * Return: Always 0.
+ * main - check the program
+ * @argc : arguments cant
+ * @argv: vector arguments
+ * Return: 0 if false, something else otherwise.
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int (*get)(int, int);
+	int a, b, operator;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(989);
+		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
-		exit(999);
+		exit(99);
 	}
-
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	operator = argv[2][0];
+	switch (operator)
 	{
-		printf("Error\n");
-		exit(100);
+		case '+':
+			break;
+		case '-':
+			break;
+		case '*':
+			break;
+		case '/':
+			break;
+		case '%':
+			break;
+		default:
+			printf("Error\n");
+			exit(99);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
-
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	get = get_op_func(argv[2]);
+	printf("%d\n", get(a, b));
 	return (0);
 }
