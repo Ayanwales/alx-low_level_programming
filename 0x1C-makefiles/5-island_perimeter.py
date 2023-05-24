8112118735b7d0d171d114344df512edfcb41934
Ayanwales <Ayanwalesulaymon@gmail.com>
2023-05-24 01:06:33 -0700

    added

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contain fucntion island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+    """
+    count = 0
+    if (len(grid) <= 100 and len(grid[0]) <= 100):
+        for i in grid:
+            for j in i:
+                if j == 1:
+                    count = count + 1
+        if count != 0:
+            count = count * 2 + 2
+    return count
